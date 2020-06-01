208fdada3ebeef689c8bbc227d2e0525e5a8aa3a
Ryusei Takano <s1250034@gmail.com>
2020-06-01 15:55:12 +0900

    pod 削除

--- Pods/Target Support Files/Pods-SSCustomButton-SSCustomButtonUITests/Pods-SSCustomButton-SSCustomButtonUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SSCustomButton_SSCustomButtonUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SSCustomButton_SSCustomButtonUITests
-@end
