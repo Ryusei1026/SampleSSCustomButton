5933d430f484286333245cca6629e27a1f56ac30
Ryusei Takano <s1250034@gmail.com>
2020-06-01 15:47:41 +0900

    Initial Commit

---
+++ SSCustomButton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SSCustomButton
+//
+//  Created by 高野隆正 on 2020/06/01.
+//  Copyright © 2020 高野隆正. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
